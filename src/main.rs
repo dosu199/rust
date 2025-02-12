c84a367fb41a4b25c093bc78af86503fc75e4718
dosu199 <sudo1996@gmail.com>
2025-02-12 16:27:58 +0100

    added code for linked-list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Node<'a> {
+    val: i32,
+    next: Option<&'a Box<Node<'a>>>,
+}
+
+impl Node<'_>  {
+    fn new(val: i32) -> Node<'static>  {
+        Node { val, next: None }
+    }
+
+    fn add_front(nodes: &mut Vec<Box<Node>>, val: i32) {
+        let mut new_node = Box::new(Node::new(val));
+        new_node.next = nodes.get(0);
+
+    //treba ubaciti sad novi node(new_node) u nodes Vec
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut nodes: Vec<Box<Node>> = Vec::new();
+    nodes.insert(0, Box::new(Node::new(1)));
+   
+
+    for i in 2..4  {
+        Node::add_front(&mut nodes,i);
+    }
+
 }
